86455b1eb0632f54707fee300b41a8d75657467a
Subhrajit Debnath <sdebnath@jahnelgroup.com>
2021-03-07 00:36:21 -0800

    LeetCode Daily Challenge: 2021-03-06 Solution
    Pretty terrible brute force solution. I was able to grasp that presorting the words by length in reverse order helped with runtime, but I completely missed that my inner check could have been improved greatly if I started to preform the encoded string as it would be in a simple in check rather that the current 2nd loop. Other smarter solutions seemed to be using a trie data structure which was also completely missed by me.

---
+++ leetcode/daily_challenge/day_6_q_1.py
@@ -0,0 +1,18 @@
+# Short Encoding of Words
+# https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/588/week-1-march-1st-march-7th/3662/
+
+class Solution:
+    def minimumLengthEncoding(self, words: List[str]) -> int:
+        sorted_words = sorted(words, key=lambda x:len(x), reverse=True)
+        included_values = []
+        
+        for word in sorted_words:
+            should_be_included = True
+            for included in included_values:
+                if included.endswith(word):
+                    should_be_included = False
+            if should_be_included:
+                included_values.append(word)
+        
+        encoded_string = '#'.join(included_values)
+        return len(encoded_string) + 1
