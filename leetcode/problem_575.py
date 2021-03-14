cf85f312ca94aed14dd1144cd7d77f9571346332
Subhrajit Debnath <sdebnath@jahnelgroup.com>
2021-03-13 21:29:56 -0700

    Restructure 575

--- leetcode/problem_575.py
+++ leetcode/problem_575.py
@@ -1,5 +1,6 @@
-# Distribute Candies
-# https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/588/week-1-march-1st-march-7th/3657/
+# 575. Distribute Candies
+# Difficulty: Easy
+# https://leetcode.com/problems/distribute-candies/
 
 class Solution:
     def distributeCandies(self, candy_types: List[int]) -> int:
